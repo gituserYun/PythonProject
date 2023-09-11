96d8155f832183f2134d7494e4983cb8eb9a9754
LeeTaeYun <lty0812@naver.com>
2023-09-11 21:08:11 +0900

    update(2309112109)

--- Protocol/directory_scan.py
+++ Protocol/directory_scan.py
@@ -1,74 +1,84 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 import os
 
-#test_url: "http://192.168.107.128/"
-target_url = "http://192.168.107.130/"
-response = requests.get(target_url)
-
-#set type filenames
+#set()타입 지정&방문할 url 지정
 filenames_set = set()
+visited = set()
 
-#check if the request to the target url
-st_code = response.status_code
-if st_code == 200:
-    #HTML content of the page
-    soup = BeautifulSoup(response.text, 'html.parser')
+#서버 디렉터리 탐색
+def crawl(target_url):
+    #방문 url 추가
+    visited.add(target_url)
+    response = requests.get(target_url)
     
-    #[script, a, ...]
-    #find the 
-    script_tags = soup.find_all('script')
-    a_tags = soup.find_all('a')
-    print(script_tags)
-    # print()
-    # print(a_tags)
+    #대상 url에 대한 요청 성공 여부 확인
+    st_code = response.status_code
     
-    #match[location.href, href, src, ]
-    for script_tag in script_tags:
-        # match 'src'
-        match_src = script_tag.get('src')
-        if match_src:
-            match = re.search(r'[^/]+$', match_src)
-            if match:
-                filename = match.group(0)
-                filenames_set.add(filename)           
-# print result
-# print("Filename(script_src): ")
-# for filename in filenames_set:
-#     print(f"-> {filename}")
-# print(len(filenames_set))
-    
-print("Filename(script_location): ")
+    if st_code == 200:
+        #페이지의 HTML
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        #태그 검사
+        script_tags = soup.find_all('script')
+        a_tags = soup.find_all('a')
+        form_tags = soup.find_all('form')
+
+        #script(location.href, src)
+        for script_tag in script_tags:
+            if script_tag.string:
+                match_location= re.search(r'location.href\s*=\s*[\'"](.*?)[\'"]', script_tag.string)
+                if match_location and not match_location.group(1).startswith('#'):
+                    url_without_query_string= urlparse(match_location.group(1)).path  
+                    filename_only= os.path.basename(url_without_query_string)
+                    filenames_set.add(filename_only)
+
+            match_src= script_tag.get('src')
+            if match_src and not 'ionicons' in match_src:
+                filename_only= os.path.basename(match_src)
+                filenames_set.add(filename_only)
+
+        # a(href)
+        for a_tag in a_tags:
+            href_value = a_tag.get('href')
+            if href_value and not href_value.startswith('#'):
+                full_url = urljoin(target_url, href_value)
+                
+                url_without_query_string = urlparse(href_value).path  
+                filename_only = os.path.basename(url_without_query_string)
+                filenames_set.add(filename_only)   
+
+                parsed_url = urlparse(full_url)
+                
+                #추출한 문자열이 올바른 파일 이름인지 검사
+                match_valid_filename = re.match(r'^[\w,\s-]+\.[A-Za-z0-9]+$', filename_only)
+                if match_valid_filename:
+                    filenames_set.add(filename_only) 
+
+                parsed_url = urlparse(full_url)
+                
+                #URL이 'http' 또는 'https'인지 확인합니다
+                if parsed_url.scheme in ['http', 'https']:
+                    if full_url not in visited:  #페이지를 방문하지 않은 경우
+                        crawl(full_url)  #페이지를 방문하는 재귀 호출문
+
+        for form_tag in form_tags:
+            action_value=form_tag.get('action')
+            if action_value and not action_value.startswith('#'):
+                url_without_query_string=urlparse(action_value).path  
+                filename_only=os.path.basename(url_without_query_string)
+                filenames_set.add(filename_only)
+
+    else:  #200 상태 코드가 아닌 부분
+        print(f"Failed to retrieve {target_url}. Status code: {response.status_code}")
+
+#start crawling
+crawl("http://192.168.107.128/bbs.jsp")
+
+#print result.
+print("Filename: ")
 for filename in filenames_set:
-    print(f"-> {filename}")
+      print(f"-> {filename}")
 print(len(filenames_set))
-    
-    # for script_tag in script_tags:
-    #     match = re.search(r'location.href\s*=\s*[\'"](.*?)[\'"]', script_tag.string)
-    #     if match:
-    #         redirect_url = match.group(1)
-
-    #         full_url = urljoin(target_url, redirect_url)
-    #         filename_only = os.path.basename(full_url)
-            
-    #         #print(f"Redirecting to: {full_url}")
-    #         print(f"Filename(script): {filename_only}")
-            
-#     for a_tag in a_tags:
-#         # match 'href'
-#         match_href = a_tag.get('href')
-#         if match_href:
-#             match = re.search(r'[^/]+$', match_href)
-#             if match:
-#                 filename = match.group(0)
-#                 filenames_set.add(filename)
-# else:
-#     print(f"Failed to retrieve {target_url}. Status code: {response.status_code}")
-
-# # print result
-# print("Filename(a_href): ")
-# for filename in filenames_set:
-#     print(f"-> {filename}")
-# print(len(filenames_set))
