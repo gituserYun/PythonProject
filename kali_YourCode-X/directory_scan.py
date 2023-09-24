cfa5376cca212f8ad4b9e4d780b50b7c312e6d07
LeeTaeYun <lty0812@naver.com>
2023-09-24 10:54:12 +0900

    update kali_YourCode-X

--- kali_YourCode-X/directory_scan.py
+++ kali_YourCode-X/directory_scan.py
@@ -31,7 +31,7 @@ def dirSearch(target_url):
                     match_location = re.search(r'(window\s*\.\s*)?location\s*(\.\s*href|\.\s*replace)\s*(=\s*[\'"](.*?)[\'"]|\([\'"](.*?)[\'"]\))', tag.string, re.IGNORECASE)
                     if match_location and (match_location.group(4) or match_location.group(5)):
                         full_url = urljoin(target_url, match_location.group(4) or match_location.group(5))
-                        print(f"script_location: {full_url}")
+                        #print(f"script_location: {full_url}")
                         if not full_url.startswith('#') and full_url not in visited:
                             dirSearch(full_url) #리다이렉트된 URL 재귀 호출
 
@@ -39,7 +39,7 @@ def dirSearch(target_url):
                 if match_src and not 'ionicons' in match_src:
                     full_url = urljoin(target_url, match_src)
                     path_with_extension = urlparse(full_url).path
-                    print(f"script_src: {full_url}")
+                    #print(f"script_src: {full_url}")
 
                     #확장된 경로가 그룹화된 참조를 저장하는 사전 
                     if path_with_extension not in refer_dict:
@@ -51,7 +51,7 @@ def dirSearch(target_url):
                 href_value = tag.get('href')
                 if href_value and not href_value.startswith('#'):
                     full_url = urljoin(target_url, href_value)
-                    print(f"a_href: {full_url}")
+                    #print(f"a_href: {full_url}")
 
                     path_with_extension = urlparse(full_url).path
                     refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
@@ -71,7 +71,7 @@ def dirSearch(target_url):
                 action_value = tag.get('action')
                 if action_value and not action_value.startswith('#'):
                     full_url = urljoin(target_url, action_value)
-                    print(f"form_action: {full_url}")
+                    #print(f"form_action: {full_url}")
                     path_with_extension = urlparse(full_url).path
                     refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
 
@@ -79,7 +79,7 @@ def dirSearch(target_url):
                 href_value = tag.get('href')
                 if href_value and not href_value.startswith('#'):
                     full_url = urljoin(target_url, href_value)
-                    print(f"link_href: {full_url}")
+                    #print(f"link_href: {full_url}")
                     path_with_extension = urlparse(full_url).path
                     refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
 
@@ -87,7 +87,7 @@ def dirSearch(target_url):
                 src_value = tag.get('src')
                 if src_value and not src_value.startswith('#'):
                     full_url = urljoin(target_url, src_value)
-                    print(f"img_src: {full_url}")
+                    #print(f"img_src: {full_url}")
                     path_with_extension = urlparse(full_url).path
                     refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
             #area 태그 관련(이미지 맵의 영역을 정의)
@@ -95,7 +95,7 @@ def dirSearch(target_url):
                 href_value = tag.get('href')
                 if href_value and not href_value.startswith('#'):
                     full_url = urljoin(target_url, href_value)
-                    print(f"area_href: {full_url}")
+                    #print(f"area_href: {full_url}")
                     path_with_extension = urlparse(full_url).path
                     refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
             #meta 태그 관련(웹 페이지에 대한 정보를 포함한 곳, 특정 상황 리다이렉션 용도)
@@ -106,7 +106,7 @@ def dirSearch(target_url):
                     match_meta_refresh = re.search(r'url\s*=\s*(.*)$', content_value.strip(), re.IGNORECASE)
                     if match_meta_refresh:
                         full_url = urljoin(target_url, match_meta_refresh.group(1))
-                        print(f"meta: {full_url}")
+                        #print(f"meta: {full_url}")
                         path_with_extension = urlparse(full_url).path
                         refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
             #embed, object 태그 관련(외부 멀티미디어 콘텐츠)
@@ -114,7 +114,7 @@ def dirSearch(target_url):
                 data_or_src_attr_val = tag.get('data') or tag.get('src')
                 if data_or_src_attr_val and not data_or_src_attr_val.startswith('#'):
                     full_url = urljoin(target_url,data_or_src_attr_val)
-                    print(f"embed&object: {full_url}")
+                    #print(f"embed&object: {full_url}")
                     path_with_extension = urlparse(full_url).path
                     refer_dict.setdefault(path_with_extension, []).append((full_url, response.status_code))
 
@@ -149,4 +149,27 @@ if __name__ == "__main__":
                 directory_names.add(directory_name)
 
     for dirname in directory_names:
-        print(f"{dirname}")    
+        print(f"{dirname}")
+
+    print("\nFilename:")
+    print("===========")
+    web_extensions = {'.html', '.htm', '.php', '.jsp', '.asp', '.aspx',
+                    '.css', '.js',
+                    '.png', '.jpg', '.jpeg', '.svg'}
+
+    #main으로 리턴해줄 해당 디렉토리 경로
+    re_path = []
+    for path_with_extension, references in refer_dict.items():
+        _, ext = os.path.splitext(path_with_extension)
+
+        if ext in web_extensions:
+            re_path.append(path_with_extension)
+            print(f"{path_with_extension}")
+            #print(f"-> {path_with_extension}")
+            unique_references = set()  #set 타입을 사용하여 고유 참조 확인
+            # for ref_info in references:
+            #     full_url = ref_info[0]
+            #     status_code = ref_info[1]
+            #     unique_references.add((full_url, status_code))
+            # for ref_info in unique_references:
+            #     print(f"refer) {ref_info[0]}, Status Code: {ref_info[1]}")    
